d39cb552f28aa5102bc8a6d29e2f23a177e547fd
Daniil D <daniel217032001@gmail.com>
2026-05-31 23:27:36 +0300

    Update iris schema

--- api/endpoints/iris/schemas.py
+++ api/endpoints/iris/schemas.py
@@ -4,10 +4,22 @@ from pydantic import BaseModel, ConfigDict, Field
 class IrisFeaturesRequest(BaseModel):
     model_config = ConfigDict(populate_by_name=True)
 
-    sepal_length_cm: list[float] = Field(alias="sepal length (cm)")
-    sepal_width_cm: list[float] = Field(alias="sepal width (cm)")
-    petal_length_cm: list[float] = Field(alias="petal length (cm)")
-    petal_width_cm: list[float] = Field(alias="petal width (cm)")
+    sepal_length: list[float] = Field(
+        validation_alias="sepal_length",
+        serialization_alias="sepal length (cm)",
+    )
+    sepal_width: list[float] = Field(
+        validation_alias="sepal_width",
+        serialization_alias="sepal width (cm)",
+    )
+    petal_length: list[float] = Field(
+        validation_alias="petal_length",
+        serialization_alias="petal length (cm)",
+    )
+    petal_width: list[float] = Field(
+        validation_alias="petal_width",
+        serialization_alias="petal width (cm)",
+    )
 
 
 class TaskQueuedResponse(BaseModel):
